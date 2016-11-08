5209ba8210fd60cdfcf4a70d1f451d7bdda940e5
Fumio SAGAWA <fumio.sagawa@gmail.com>
2016-11-08 19:02:07 +0900

    add routing

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { IssueTrackerPage } from './app.po';
+
+describe('issue-tracker App', function() {
+  let page: IssueTrackerPage;
+
+  beforeEach(() => {
+    page = new IssueTrackerPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { IssueComponent } from './issue/issue.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+import { routing, appRoutingProviders }  from './app.routes';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    IssueComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
+  ],
+  providers: [appRoutingProviders],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
